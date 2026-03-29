5b8b0d48bf861039e5aac40129e1526487bb3089
Lunasgogoi <lunasgogoi98@gmail.com>
2026-03-29 23:22:51 +0530

    gitignore added

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
+
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+//database connect
+database.connect();
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:3000"
+    })
+);
+app.use(
+    fileUpload({
+        useTempFiles: true
+    })
+);
+
+//clouidinary connect
+cloudinaryConnect();
+
+//routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/course", courseRoutes);
+
+//def routes
+
+app.get("/", (req,res) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is running"
+    })
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
