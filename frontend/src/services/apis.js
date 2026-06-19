4a8faea1d544a70fd2ee54a513918203168d12e3
Lunasgogoi <lunasgogoi98@gmail.com>
2026-06-19 17:31:17 +0530

    deploying

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,5 +1,5 @@
 // src/services/apis.js
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:4000/api/v1";
+const BASE_URL = import.meta.env.VITE_BASE_URL || "https://studynotion-emet.onrender.com/api/v1";
 
 // COURSE ENDPOINTS
 export const courseEndpoints = {
